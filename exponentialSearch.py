5cfe7ea06839d368d7c1d50f0e9c2c00055a3307
embydextrous <arjitagarwal099@gmail.com>
2020-09-15 15:00:29 +0530

    Exponential Search Implementation

---
+++ exponentialSearch.py
@@ -0,0 +1,24 @@
+# Useful for binary search if array size is too large
+
+def exponentialSearch(a, x):
+    n = len(a)
+    if a[0] == x:
+        return 0
+    i = 1
+    while i < n and a[i] < x:
+        i *= 2
+    return binarySearch(a, i / 2, min(i, n), x)
+
+def binarySearch(a, l, r, x):
+    if l > r:
+        return -1
+    mid = (l+r) / 2
+    if a[mid] == x:
+        return mid
+    if a[mid] > x:
+        return binarySearch(a, l, mid - 1, x)
+    return binarySearch(a, mid + 1, r, x)
+
+a = [i for i in range(10000000)]
+print exponentialSearch(a, 93988)
+    
--- jumpSearch.py
+++ jumpSearch.py
@@ -8,9 +8,9 @@ def jumpSearch(a, key):
     prev, i = 0, step - 1
     while i < n and a[i] <= key:
         prev, i = i, i + step
-    for j in range(step):
-        if prev + j < n and a[prev + j] == key:
-            return prev + j
+    for j in range(prev, min(prev + step, n)):
+        if a[j] == key:
+            return j
     return -1
 
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
