99efb4e3309dd8af9471d7ddc90fbc5fca88cdfe
embydextrous <arjitagarwal099@gmail.com>
2020-09-16 14:25:48 +0530

    Recursive linear search

--- linearSearch.py
+++ linearSearch.py
@@ -4,6 +4,16 @@ def linearSearch(a, k):
             return i
     return -1
 
+def recursiveLinearSearch(a, l, r, x):
+    if l > r:
+        return -1
+    if a[l] == x:
+        return l
+    if a[r] == x:
+        return r
+    return recursiveLinearSearch(a, l + 1, r - 1, x)
+
 a = [1, 2, 3, 4, 5]
 for i in range(9):
     print linearSearch(a, i)
+    print recursiveLinearSearch(a, 0, len(a) - 1, i)
