9e927074be545a56508aa2f3d0ff9de32519a7cb
embydextrous <arjitagarwal099@gmail.com>
2020-09-17 12:20:37 +0530

    Find floor of x in a sorted array

--- ceilingAndFloor.py
+++ ceilingAndFloor.py
@@ -22,6 +22,31 @@ def ceil(a, l, r, x):
         return ceil(a, m+1, r, x)
     return ceil(a, l, m - 1, x)
 
+def findFloor(a, x):
+    n = len(a)
+    if n == 0:
+        return -1
+    if x >= a[n-1]:
+        return a[n-1]
+    if x < a[0]:
+        return -1
+    return a[floor(a, 0, n - 1, x)]
+
+def floor(a, l, r, x):
+    if l > r:
+        return
+    m = l + (r-l) / 2
+    if a[m] == x:
+        return m
+    if m > l and a[m-1] < x and a[m] > x:
+        return m - 1
+    if m < r and a[m] < x and a[m+1] > x:
+        return m
+    if a[m] < x:
+        return floor(a, m + 1, r, x)
+    return floor(a, l, m - 1, x)
+
 a = [1, 3, 4, 6, 8, 9, 10]
 for i in range(12):
-    print findCeiling(a, i)
+    #print findCeiling(a, i)
+    print findFloor(a, i)
