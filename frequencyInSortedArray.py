c991dde474ba891076e070ab5ba0a564e790f79b
embydextrous <arjitagarwal099@gmail.com>
2020-09-18 12:05:32 +0530

    Refine frequency problem

--- frequencyInSortedArray.py
+++ frequencyInSortedArray.py
@@ -3,8 +3,10 @@ def findFrequency(a, x):
     if n == 0:
         return 0
     r = getRightIndex(a, 0, n - 1, x)
+    if r == -1:
+        return 0
     l = getLeftIndex(a, 0, n - 1, x)
-    if l == -1 or r == -1:
+    if l == -1:
         return 0
     return r - l + 1
 
@@ -12,30 +14,22 @@ def getRightIndex(a, l, r, x):
     if l > r:
         return -1
     mid = l + (r - l) / 2
-    if l == r and a[r] == x:
-        return r
-    if mid < r and a[mid] == x and a[mid + 1] > x:
+    if (mid == len(a)-1 or x < a[mid+1]) and a[mid] == x:
         return mid
-    if mid < r and a[mid] == x and a[mid + 1] == x:
-        return getRightIndex(a, mid + 1, r, x)
-    if a[mid] < x:
-        return getRightIndex(a, mid + 1, r, x)
-    return getRightIndex(a, l, mid - 1, x)
+    if x < a[mid]:
+        return getRightIndex(a, l, mid - 1, x)
+    return getRightIndex(a, mid + 1, r, x)
 
 def getLeftIndex(a, l, r, x):
     if l > r:
         return -1
     mid = l + (r - l) / 2
-    if l == r and a[r] == x:
-        return r
-    if mid > l and a[mid] == x and a[mid - 1] < x:
+    if (mid == 0 or x > a[mid-1]) and a[mid] == x:
         return mid
-    if mid > l and a[mid] == x and a[mid - 1] == x:
-        return getLeftIndex(a, l, mid - 1, x)
-    if a[mid] < x:
-        return getRightIndex(a, mid + 1, r, x)
+    if x > a[mid]:
+        return getLeftIndex(a, mid + 1, r, x)
     return getLeftIndex(a, l, mid - 1, x)
 
-a = [1, 2, 2, 3]
+a = [1, 2, 2, 2, 2, 2, 3]
 for i in range(5):
     print findFrequency(a, i)
