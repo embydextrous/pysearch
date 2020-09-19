f3b78c2f26ebca58aa5e5a74b20385db3f468914
embydextrous <arjitagarwal099@gmail.com>
2020-09-19 21:27:08 +0530

    Kth Smallest using Quick Select

--- kthLargestOrSmallestElement.py
+++ kthLargestOrSmallestElement.py
@@ -62,9 +62,49 @@ def findKthSmallestMaxHeap(a, k):
             maxHeapify(a, k, 0)
     return a[0]
 
+def quickSelect(a, l, r, k):
+    if l < r:
+        pivot = partition(a, l, r)
+        if pivot == k - 1:
+            return a[pivot]
+        if pivot < k:
+            return quickSelect(a, pivot + 1, r, k)
+        return quickSelect(a, l, pivot - 1, k)
+
+def partition(a, l, r):
+    pivot = a[l]
+    i, j = l + 1, r
+    while i < j:
+        while i <= r and a[i] <= pivot:
+            i += 1
+        while j > l and a[j] >= pivot:
+            j -= 1
+        if i < j:
+            a[i], a[j] = a[j], a[i]
+    a[l], a[j] = a[j], a[l]
+    return j
+
+def partition(a, l, r):
+    i = l - 1    
+    i, j = l + 1, r
+    while i < j:
+        while i <= r and a[i] <= a[l]:
+            i += 1
+        while j > l and a[j] >= a[l]:
+            j -= 1
+        if i < j:
+            a[i], a[j] = a[j], a[i]
+    a[l], a[j] = a[j], a[l]
+    return j
+
+def findKthSmallestQuickSelect(a, k):
+    return quickSelect(a, 0, len(a) - 1, k)
+
 a = [10, 8, 2, 12, 17, 4, 1, 19, 15, 16, 11]
 print sorted(a)
 print findKthLargestMinHeap(a, 4)
 print findKthLargestMaxHeap(a, 4)
 print findKthSmallestMinHeap(a, 4)
 print findKthSmallestMaxHeap(a, 4)
+print a
+print findKthSmallestQuickSelect(a, 6)
